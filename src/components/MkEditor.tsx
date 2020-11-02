2dc41e43a10f67d2684a4808e78c97f1083ad217
xingbo.li <1711293058@qq.com>
2020-11-02 18:53:17 +0800

    上传遮罩样式优化

--- src/components/MkEditor.tsx
+++ src/components/MkEditor.tsx
@@ -80,10 +80,12 @@ const MkEditorComponent = defineComponent({
     },
     render() {
         let loadingHtml = (
-            <div class="absolute flex items-center z-50 justify-center w-full h-full bg-black bg-opacity-25" >
-                <div class="rounded-full animate-spin  bg-gradient-to-r from-orange-400 to-red-500   h-20 w-20 flex items-center justify-center">
-                    <div class="rounded-full bg-black bg-opacity-25 h-16 w-16 flex items-center justify-center text-white text-xs">
-                        上传中
+            <div class="absolute flex flex-col items-center z-50 justify-center w-full h-full bg-black bg-opacity-25" >
+                <div class="text-sm mb-1 text-orange-800">
+                    图片正在上传中...
+                </div>
+                <div class="rounded-full animate-spin opacity-50  bg-gradient-to-r from-black to-white   h-12 w-12 flex items-center justify-center">
+                    <div class="rounded-full bg-white bg-opacity h-10 w-10 flex items-center justify-center text-white text-xs">
                     </div>
                 </div>
             </div>
